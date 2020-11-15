89736a26d3b5e8c58b6a201e14e2374d3785f759
FidelM345 <fidelomolo7@gmail.com>
2020-11-15 18:07:01 +0300

    my second commit

--- app/src/main/java/com/example/kotlincourotines/MainActivity.kt
+++ app/src/main/java/com/example/kotlincourotines/MainActivity.kt
@@ -13,18 +13,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+
+
+         Log.d(Constant.TAG, "Thread launched in main2 thread: ${Thread.currentThread().name}")
+    }
+
+
+    //sample function for simple couroutine calls
+    fun simpleCoRoutineCall(){
         //the coroutines life is tied to the application life duration. Simplest way to launch a coroutine
         GlobalScope.launch {
 
             delay(3000L)//similar to the sleep function in threads used to delay the coroutine for 3 s. Will only delay the sepecifc coroutine and not the entire thread
             Log.d(Constant.TAG, "Thread launched in Coroutine: ${Thread.currentThread().name}")
 
-                //The coroutine is also terminated when the main thread terminates
+            //The coroutine is also terminated when the main thread terminates
         }
-
-         Log.d(Constant.TAG, "Thread launched in main2 thread: ${Thread.currentThread().name}")
     }
-
 
 
 }
